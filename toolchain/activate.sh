fa2c3e43f48e7d47e705878503dce2bcd5de74ae
Ricardo JL Rufino <ricardo@edu3.com.br>
2022-03-01 22:26:19 -0300

    add infors in activete toolchain

--- toolchain/activate.sh
+++ toolchain/activate.sh
@@ -1,13 +1,21 @@
  
-# ANYKA SETUP variables
-# BY RJLR
+# ANYKA SETUP make variables
+
 
 export TOOLCHAIN_PATH=/media/ricardo/Dados/Outros/Linux/tools/arm-anykav200-crosstool/usr
 
 echo "Using Toolchain: $TOOLCHAIN_PATH"
+if [[ ! -d $TOOLCHAIN_PATH ]]; then
+    echo "Toolchain not exist ! Please download from https://github.com/ricardojlrufino/arm-anykav200-crosstool"
+    echo "and configure TOOLCHAIN_PATH on ${BASH_SOURCE[0]}"
+    exit -1
+fi
+
+
 
 export CROSSPATH=$TOOLCHAIN_PATH/bin
 export STAGING_DIR=$TOOLCHAIN_PATH
+
 export PATH="$CROSSPATH:$PATH"
 export LD_LIBRARY_PATH=$STAGING_DIR/lib:$LD_LIBRARY_PATH
 
@@ -22,6 +30,12 @@ export LD=${CROSSPREFIX}ld
 export AS=${CROSSPREFIX}as
 export AR=${CROSSPREFIX}ar
 export RANLIB=${CROSSPREFIX}ranlib
+export LINK=${CROSSPREFIX}g++
+
+
+echo "GCC Version $(${CC} -dumpversion)"
+
+## HELPER FUNTIONS FOR SCRIPTS ###
 
 ## import functions do deploy
 DIR=$(dirname "${BASH_SOURCE[0]}")   # Get the directory name
