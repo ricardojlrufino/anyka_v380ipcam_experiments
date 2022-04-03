12b50c1d6e8a5ee552014f8b27ebfedce8430b1c
Ricardo JL Rufino <ricardo@edu3.com.br>
2022-04-03 10:26:12 -0300

    add display cleanup

--- apps/ak_test_display/build.sh
+++ apps/ak_test_display/build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash 
 
 # Read enviroment
-export TOOLCHAIN_PATH=/media/ricardo/Dados/Outros/Linux/tools/arm-anykav200-crosstool/usr
+# set SDK
+source ../../toolchain/activate.sh
 
 export PLAT_WORKDIR=/media/ricardo/Dados/Outros/Linux/IPCAM/Camera-ANYKA-v380/anyka_v380ipcam_experiments/akv300-extract
 export LIBDISPLAY_PATH=/media/ricardo/Dados/Outros/Linux/IPCAM/Camera-ANYKA-v380/TEST_SRCS/ssd1306
@@ -11,45 +13,18 @@ TARGET_PATH=/mnt/sdcard/CAM/scripts
 export TARGET_BIN=ak_test_display
 
 
-echo "Using Toolchain: $TOOLCHAIN_PATH"
-
-export CROSSPATH=$TOOLCHAIN_PATH/bin
-export STAGING_DIR=$TOOLCHAIN_PATH
-export PATH="$CROSSPATH:$PATH"
-export LD_LIBRARY_PATH=$STAGING_DIR/lib_v380:$STAGING_DIR/lib:$LD_LIBRARY_PATH
-
-export TARGET=arm-anykav200-linux-uclibcgnueabi
-export CROSS=arm-anykav200-linux-uclibcgnueabi
-export BUILD=x86_64-pc-linux-gnu
-export CROSSPREFIX=${CROSS}-
-export STRIP=${CROSSPREFIX}strip
-export CXX=${CROSSPREFIX}g++
-export CC=${CROSSPREFIX}gcc
-export LD=${CROSSPREFIX}ld
-export AS=${CROSSPREFIX}as
-export AR=${CROSSPREFIX}ar
-export RANLIB=${CROSSPREFIX}ranlib
-
 make clean
 
-make 
-
-file $TARGET_BIN
+make || exit $?
 
 if [ -f "$TARGET_BIN" ]; then
     echo "Uploading to $TARGET_DEVICE:$TARGET_PATH"
-    # scp $BIN $TARGET_DEVICE:$TARGET_PATH
+ 
+	send_ftp ${TARGET_DEVICE} ${TARGET_BIN} ${TARGET_PATH}
 
-ftp ${TARGET_DEVICE} <<EOF 
-pwd
-cd ${TARGET_PATH}
-ls
-put ${TARGET_BIN}
-chmod +x ${TARGET_BIN}
-ls
-EOF
     echo "-- DONE --"
 else
     echo "-- ERROR --"
 fi
+
 
